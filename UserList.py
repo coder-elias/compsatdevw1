ef754d4eb1e735ddc39249a0fd1b6fd7db8a7b32
migzu <luis.bandelaria@obf.ateneo.edu>
2020-12-10 18:49:01 +0800

    deleted unnecessary files
    will push to another branch

--- UserList.py
+++
@@ -1,8 +0,0 @@
-#Read list of usernames and passwords from UserList.txt
-users = []
-userListFile = open("UserList.txt")
-
-for line in userListFile:
-    
-    line = line.strip("\n")
-    users.append(tuple(line.split(":")))
