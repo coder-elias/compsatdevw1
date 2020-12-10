e7815fff033ff9334eb56e3affb873bfa18efd36
migzu <luis.bandelaria@obf.ateneo.edu>
2020-12-10 18:54:15 +0800

    cleaned master branch
    to merge cleaning

--- LogInInput.py
+++
@@ -1,3 +0,0 @@
-#User input for username and password
-username = input(" ---------------------------------------------------\n | Hello! Welcome to Dev Workshop #1: Git and Notion |\n --------------------------------------------------- \nUsername: ")
-password = input("Password: ")
