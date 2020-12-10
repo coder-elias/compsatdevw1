7ff9439fef00c00cba35bf593dffb85bb7eb6d82
migzu <luis.bandelaria@obf.ateneo.edu>
2020-12-10 18:34:03 +0800

    initial commit

---
+++ LogInInput.py
@@ -0,0 +1,3 @@
+#User input for username and password
+username = input(" ---------------------------------------------------\n | Hello! Welcome to Dev Workshop #1: Git and Notion |\n --------------------------------------------------- \nUsername: ")
+password = input("Password: ")
